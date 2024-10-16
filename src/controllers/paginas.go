3f2498f3a05fd7abfaf60eed76cd7520f6723901
Pedro Souza <pedrops244@hotmail.com>
2024-10-16 11:27:53 -0300

    auth middleware and create home page

--- src/controllers/paginas.go
+++ src/controllers/paginas.go
@@ -8,7 +8,11 @@ import (
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
+
 func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "cadastro.html", nil)
+}
 
+func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "home.html", nil)
 }
