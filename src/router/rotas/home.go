f674d36cf88569843f2663e7830b3bbee3b91f4f
Pedro Souza <pedrops244@hotmail.com>
2024-10-17 10:57:55 -0300

    ..

--- src/router/rotas/home.go
+++ src/router/rotas/home.go
@@ -9,5 +9,5 @@ var rotaPaginaPrincipal = Rota{
 	URI:                "/home",
 	Metodo:             http.MethodGet,
 	Funcao:             controllers.CarregarPaginaPrincipal,
-	RequerAutenticacao: true,
+	RequerAutenticacao: false,
 }
